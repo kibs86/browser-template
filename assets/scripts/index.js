34bc6c006d4b3938ca28944ce55f5e99a825ce7b
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-12-26 22:16:01 -0500

    Add linter exceptions for materialize.
    * Added materialize to exception folders for JSCS.
    * Added materialize js files to js hint ignore.
    * Fixed linter error in index.js.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,6 @@ $(() => {
   setAPIOrigin(location, config);
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 15, // Creates a dropdown of 15 years to control year
   });
 });
