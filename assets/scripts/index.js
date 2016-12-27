3cf398566dbdbc0be62f3445201b7bdec11f1f14
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-12-26 21:46:52 -0500

    Remove example files.
    * Removed example script and testing files.
    * Removed reference to examples from js manifest.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,9 +10,3 @@ $(() => {
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 });
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example');
-
-// use require without a reference to ensure a file is bundled
-require('./example');
